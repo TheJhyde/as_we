6eca6a15b959e469d500ebd2235569315dbf7ba3
J Hyde <jkhyde86@gmail.com>
2018-09-06 09:53:25 -0500

    ActionCable configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://asweknowit.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://asweknowit.herokuapp.com', 'https://asweknowit.herokuapp.com/games']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
