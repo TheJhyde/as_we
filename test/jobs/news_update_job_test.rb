670c5732290c3c51ead6fcf81d29078cc9fd650f
J Hyde <jkhyde86@gmail.com>
2018-09-24 11:42:45 -0500

    Job test

--- test/jobs/news_update_job_test.rb
+++ test/jobs/news_update_job_test.rb
@@ -3,7 +3,14 @@
 require "test_helper"
 
 class NewsUpdateJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "perform" do
+    assert_difference 'Message.count', 2 do
+      NewsUpdateJob.perform_now([players(:player_2), players(:player_3)], "TEST MESSAGE")
+    end
+
+    player = players(:player_2)
+    convo = player.find_conversation(players(:hrn))
+    message = convo.messages.order(:created_at).last
+    assert_equal "TEST MESSAGE", message.contents
+  end
 end
